78c89c6bed8e0ecaa3c52a2acbd1606ded39b6dc
BarbosaRT <69812923+BarbosaRT@users.noreply.github.com>
2026-04-04 12:21:01 -0300

    Change for UI canvas

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: match-libras
 - Unity version: Unity 6000.3.11f1
 - Active game object:
-  - Name: Level Manager
+  - Name: Image
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     [field: SerializeField] public List<Sprite> NumberSprites { get; private set; }
-    public SpriteRenderer spriteRenderer;
+    public Image spriteRenderer;
     public int number;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
